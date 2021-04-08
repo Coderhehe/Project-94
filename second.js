bd6b99ca36ec3b8bf26685be63089867181e2cd2
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-04-08 12:28:12 -0400

    Add files via upload

---
+++ second.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCCCdKhTaHUSIOUpSl7mSgMdS2XP_RPShQ",
+    authDomain: "project-93-96-62dbe.firebaseapp.com",
+    databaseURL: "https://project-93-96-62dbe-default-rtdb.firebaseio.com",
+    projectId: "project-93-96-62dbe",
+    storageBucket: "project-93-96-62dbe.appspot.com",
+    messagingSenderId: "833404659211",
+    appId: "1:833404659211:web:09bd27cf8fe38abb73c90e"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
